5e83b47d01ca69738f2e83262dc7baa0fa4a869c
Pedro1949 <142362192+Pedro1949@users.noreply.github.com>
2023-11-14 20:11:38 -0500

    Add files via upload

---
+++ src/Carro.java
@@ -0,0 +1,79 @@
+public class Carro {
+    // Atributos
+    private String modelo;
+    private String color;
+    private boolean pinturaMetalizada;
+    private String placa;
+    private String tipoCarro;
+    private int anoFabricacion;
+    private String modalidadSeguro;
+
+    // Constructor
+    public Carro(String modelo, String color, boolean pinturaMetalizada, String placa, String tipoCarro, int anoFabricacion, String modalidadSeguro) {
+        this.modelo = modelo;
+        this.color = color;
+        this.pinturaMetalizada = pinturaMetalizada;
+        this.placa = placa;
+        this.tipoCarro = tipoCarro;
+        this.anoFabricacion = anoFabricacion;
+        this.modalidadSeguro = modalidadSeguro;
+    }
+
+    // Métodos getter y setter (para acceder y modificar los atributos)
+    public String getModelo() {
+        return modelo;
+    }
+
+    public void setModelo(String modelo) {
+        this.modelo = modelo;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public boolean isPinturaMetalizada() {
+        return pinturaMetalizada;
+    }
+
+    public void setPinturaMetalizada(boolean pinturaMetalizada) {
+        this.pinturaMetalizada = pinturaMetalizada;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    public String getTipoCarro() {
+        return tipoCarro;
+    }
+
+    public void setTipoCarro(String tipoCarro) {
+        this.tipoCarro = tipoCarro;
+    }
+
+    public int getAnoFabricacion() {
+        return anoFabricacion;
+    }
+
+    public void setAnoFabricacion(int anoFabricacion) {
+        this.anoFabricacion = anoFabricacion;
+    }
+
+    public String getModalidadSeguro() {
+        return modalidadSeguro;
+    }
+
+    public void setModalidadSeguro(String modalidadSeguro) {
+        this.modalidadSeguro = modalidadSeguro;
+    }
+
+}
